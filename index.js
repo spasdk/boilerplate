9196eaab6638d5530e14ec6d4df71c8d25dd0f02
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-08-12 16:57:53 +0300

    rework sdk init procedure

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Main SDK entry point.
+ */
+
+'use strict';
+
+// redefinition of plugin options example
+// require('spa-plugin-static/config').develop.port = 8000;
+
+// load default plugins
+// and run default tasks
+module.exports = require('spasdk/default');
