a829666d7d44af992d994d36832f42a91c116fc9
Stanislav Kalashnik <darkpark.main@gmail.com>
2016-08-22 00:30:08 +0300

    correct example

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 'use strict';
 
 // redefinition of plugin option example
-// require('spa-plugin-static/config').develop.port = 8000;
+// require('spa-plugin-static/config').default.port = 8000;
 
 // load default plugins
 // and run default tasks
